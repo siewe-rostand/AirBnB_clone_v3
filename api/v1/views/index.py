65453b68c5f966a2cdfdaa85884bb785a6a562a5
siewe-rostand <rostand.siewe9@gmail.com>
2024-02-24 08:05:50 -0800

    5. Not found

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,20 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ returns a JSON """
     return jsonify(status="OK")
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ return number of each objects by type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
