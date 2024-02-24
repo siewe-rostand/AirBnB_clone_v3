c7d5a6bc124c21774ce6630bc6f624bb1affd7aa
siewe-rostand <rostand.siewe9@gmail.com>
2024-02-24 06:26:30 -0800

    3. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_session(exception):
     """ Closes storage session """
     storage.close()
+
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
